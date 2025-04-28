2b1e425ca8c94aea1c4413e97299ba43bcb98116
Mihajasoa <mihajasoaalain85@gmail.com>
2025-04-28 08:17:45 +0300

    add Retrofit instance for API service communication

---
+++ app/src/main/java/com/example/mybank/api/RetrofitInstance.kt
@@ -0,0 +1,18 @@
+package com.example.mybank.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import com.example.mybank.service.ClientApiService
+
+object RetrofitInstance {
+    // Ajouter le slash à la fin de l'URL
+    private const val BASE_URL = "http://192.168.43.81:3000/"
+
+    val api: ClientApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ClientApiService::class.java)
+    }
+}
