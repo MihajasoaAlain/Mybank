93fd0710974ebdb8bce46242cc8df22bd8315270
Mihajasoa <mihajasoaalain85@gmail.com>
2025-04-28 08:18:22 +0300

    add ClientApiService interface for client operations and statistics retrieval

---
+++ app/src/main/java/com/example/mybank/service/ClientApiService.kt
@@ -0,0 +1,27 @@
+package com.example.mybank.service
+
+import ClientBancaire
+import com.example.mybank.data.dto.StatsResponse
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface ClientApiService {
+    @GET("client")
+    suspend fun getClients(): List<ClientBancaire>
+
+    @POST("client")
+    suspend fun ajouterClient(@Body client: ClientBancaire)
+
+    @PUT("client/{id}")
+    suspend fun modifierClient(@Path("id") id: Int, @Body client: ClientBancaire)
+
+    @DELETE("client/{id}")
+    suspend fun supprimerClient(@Path("id") id: Int)
+
+    @GET("client/stats")
+    suspend fun getStats(): StatsResponse
+}
